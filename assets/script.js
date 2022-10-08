f35ebf734752b07d6cabb630448fee5b724d7cfc
Dahlia <rlally89@gmail.com>
2022-10-08 16:16:15 -0700

    connect search to API

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,10 @@
-
-//generate API key
+//Global Variables
 var APIKey = "a700477e931b172046d7bbc2533b8c5a";
 
-//create variable for the API to call
-var city = "White Salmon";
-
-//Construct a query URL to make call:
-//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var searchButton = $('#search-button');
+var citySearch = $('#city-search');
+var formEl = $('city-form');
+var currentWeather = $('#current-weather');
 
 //Current Date + Five Calendar Days
 var today = moment();
@@ -18,16 +15,33 @@ $("#day3").text(today.add(1, 'days').format("MMM Do, YYYY"));
 $("#day4").text(today.add(1, 'days').format("MMM Do, YYYY"));
 $("#day5").text(today.add(1, 'days').format("MMM Do, YYYY"));
 
+//API test: successful link
 
-fetch(queryURL , {
-    cache: 'reload',
-})
-    .then(function(response) {
+function getApi(cityName) {
+    $('input[type="text"]').val('');
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey + "&units=imperial";
+    
+    fetch(queryURL)
+        .then(function(response) {
         return response.json();
     })
-    .then(function (data) {
-    console.log(data);
-    console.log(data.wind.speed);
-});
+        .then(function (data) {
+        console.log(data);
+        currentWeather.append(`<h2>${data.name}</h2>`);
+        currentWeather.append(`<span>${data.main.temp}</span>`);
+        currentWeather.append(`<span>${data.wind.speed}</span>`);
+        currentWeather.append(`<span>${data.main.humidity}</span>`);
+        
+    });
+}
 
-console.log("howdy weather");
+//Click Event: Type City, Click Search, Function to console.log API results
+searchButton.on('click', function searchCitySubmit(event) {
+    event.preventDefault();
+
+    var currentCity = citySearch.val();
+    console.log('City:', citySearch.val());
+    getApi(currentCity);
+          
+});
